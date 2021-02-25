ef19d6a19da288844f8cb00d0e390e163831f66c
RogersJohn811 <rogersjohn811@gmail.com>
2021-02-25 14:57:40 -0500

    added form.js

---
+++ src/components/Form.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+export default function Form(props) {
+    let styles = {
+        margin: "15px",
+        width: "200px",
+        height: "30px"
+    }
+    return (
+      <div>
+        <form className="form">
+          <input style={styles}
+            value={props.search}
+            name="search"
+            onChange={props.handleInputChange}
+            type="text"
+            placeholder="Search for Employee"
+          />        
+        </form>
+      </div>
+    );
+}
